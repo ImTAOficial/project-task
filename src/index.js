51d9de64e8c764a6561acb5f3a15ffd6ca8698c7
Antonio Herrera <taherreraoficial@gmail.com>
2024-05-19 22:33:23 -0600

    Loggin terminado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from '../src/Components/Routes/route';
-/*import './index.css';*/
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <route />
-  </React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
